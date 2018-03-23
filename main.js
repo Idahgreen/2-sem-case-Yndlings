c86c8cb3c492039b6d13b188cb32b3fa2f7418ea
sengelbreth <sophie.engel1202@gmail.com>
2018-03-23 10:36:42 +0100

    slider

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// dette er et javascript dokument
+document.querySelector("#myRange").addEventListener("input", visTal);
+
+function visTal() {
+    console.log(this.value);
+    document.querySelector(".value").textContent = this.value;
+    document.querySelector(".pris").textContent = Math.round(this.value / 200);
+}
