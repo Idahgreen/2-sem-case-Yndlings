29c99a3aee7a6ad8579f7bf3d2f2652294318359
Ida Holm Green <31645826+Idahgreen@users.noreply.github.com>
2018-04-05 11:42:15 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#myRange").addEventListener("input", visTal);
-
-function visTal() {
-    console.log(this.value);
-    document.querySelector(".value").textContent = this.value;
-    document.querySelector(".pris").textContent = Math.round(this.value / 200);
-}
